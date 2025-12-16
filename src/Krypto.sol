5272d038ad956c9c1c66bdd60a246c429ec00d93
Mahreen <117590543+mahreenabbad@users.noreply.github.com>
2025-12-16 22:22:38 +0500

    Add changes
    Removed unnecessary blank lines in Krypto.sol

--- src/Krypto.sol
+++ src/Krypto.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-
 import "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
 import "@openzeppelin-contracts-upgradeable/contracts/token/ERC20/extensions/ERC20PausableUpgradeable.sol";
@@ -11,8 +10,6 @@ import "@openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeabl
 import "@openzeppelin-contracts-upgradeable/contracts/utils/ReentrancyGuardUpgradeable.sol";
 import "@openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
-
 
 /**
  * @title KryptoSignatures
